450a07cc2f42375068d561fee77d81dd7147dfd4
Crowdbotics <team@crowdbotics.com>
2023-07-03 21:43:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fitshore-41886",
+  url: "https://fitshore-41886.botics.co",
+  api: "https://fitshore-41886.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
